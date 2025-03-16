255bceda83c6dec934e83ce6cf83975718149c43
Musawirkorai <musawirkorai@gmail.com>
2025-03-16 15:13:02 +0500

    set

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,11 +1,10 @@
 import multer from "multer";
 const storage = multer.diskStorage({
   destination: function (req, file, db) {
-    cb(null, "/tmp/my-uploads");
+    cb(null, "./public/temp");
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, file.fieldname + "-" + uniqueSuffix);
+    cb(null, file.originalname);
   },
 });
-const upload = multer({ storage: storage });
+export const upload = multer({ storage });
