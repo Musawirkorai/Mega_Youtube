18a6ac4664cc2d7ce7df4c17f9c605f176265874
Musawirkorai <musawirkorai@gmail.com>
2025-04-17 13:00:43 +0500

    API operational — server handshake established successfully

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,10 +1,14 @@
 import multer from "multer";
 const storage = multer.diskStorage({
-  destination: function (req, file, db) {
+  destination: function (req, file, cb) {
     cb(null, "./public/temp");
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
   },
 });
+
+import cors from "cors";
+app.use(cors());
+
 export const upload = multer({ storage });
